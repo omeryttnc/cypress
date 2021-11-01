8cd7588f159b4312bda6290591b27e404dba4e93
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2021-11-01 11:22:16 +0000

    first commit

---
+++ integration/my-own-test/hello.js
@@ -0,0 +1,12 @@
+///<reference types="cypress"/>
+describe('Empty Test',()=>{
+    it('test one',()=>{
+        //throw new Error('oops')
+        cy.visit('https://codedamn.com')
+    })
+})
+
+
+
+
+
